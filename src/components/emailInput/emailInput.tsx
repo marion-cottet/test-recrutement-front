9405d2872521fceffdbe61c79568fb084d43c8ac
Marion Cottet <mcottet@cardiweb.com>
2022-03-03 13:46:19 +0100

    Feat: creation of email input

---
+++ src/components/emailInput/emailInput.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { validate } from 'email-validator';
+import providers from "./providersList"
+import '../../css/emailInput.css'
+
+const getProvidersSuggestions= (input?: string): string[] => {
+  if(input && validate(input)) { // If input is a valid email
+    // Return no providers suggestion
+    return []
+  } else {
+    const splittedInput = input?.split('@'); // split the input to check what has been written after the '@'
+    if (!splittedInput || splittedInput.length < 2) { // If there is no input or it doesn't contain an '@'
+      // Return the first 3 providers of the list
+      return providers.slice(0, 3);
+    } else {
+      // Else, filter the providers that match what the user has written after the '@'
+      const bestProviders = providers.filter(provider => provider.startsWith(splittedInput[1]));
+      // If there is no provider that match the input, return the first 3 of the list, else return the first 3 providers that match the input
+      return bestProviders.length > 0 ? bestProviders.slice(0, 3) : providers.slice(0, 3);
+    }
+  }
+}
+
+const completeEmail = (email: string, provider: string): string => {
+  if (email.indexOf('@') > 0) { // If the email contains an '@'
+    // Replace what is after the '@' by the selected provider
+    return email.replace(/@(.*)/, '@' + provider);
+  } else {
+    // Else, just append the selected provider to the email after adding an '@'
+    return email.concat('@' + provider);
+  }
+}
+
+const EmailInput: React.FC = () => {
+  const [displayedProviders, setDisplayedProviders] = React.useState<string[]>(getProvidersSuggestions());
+  const [email, setEmail] = React.useState<string>("");
+
+  return (
+    <div>
+      <input type="email" value={email} className="emailInput" onChange={(event) => {
+        setDisplayedProviders(getProvidersSuggestions(event.target.value))
+        setEmail(event.target.value)
+      }}/>
+      {
+        displayedProviders.map(
+          (provider: string, index: number) => (
+            <button key={index} className="providerSuggestion" onClick={() => {
+              setEmail(completeEmail(email,provider))
+              setDisplayedProviders([])
+            }}>@{provider}</button>
+          )
+        )
+      }
+    </div>
+  )
+}
+
+export default EmailInput
---
+++ src/components/emailInput/providersList.tsx
@@ -0,0 +1,21 @@
+const providers: string[] = [
+  "orange.fr",
+  "outlook.fr",
+  "gmail.com",
+  "hotmail.fr",
+  "outlook.com",
+  "hotmail.com",
+  "wanadoo.fr",
+  "yahoo.fr",
+  "laposte.net",
+  "yahoo.com",
+  "sfr.fr",
+  "msn.com",
+  "live.fr",
+  "free.fr",
+  "numericable.fr",
+  "bbox.fr",
+  "neuf.fr"
+]
+
+export default providers
